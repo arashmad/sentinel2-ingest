cb15b314ad44c5fa84c24594301518822121b8c4
arashmad <arashmad.dev@gmail.com>
2026-06-18 23:13:12 +0200

    feat: add Sentinel Hub provider skeleton

--- src/sentinel2_ingest/providers/__init__.py
+++ src/sentinel2_ingest/providers/__init__.py
@@ -1,4 +1,5 @@
 from sentinel2_ingest.providers.base import SceneProvider
 from sentinel2_ingest.providers.fake import FakeSceneProvider
+from sentinel2_ingest.providers.sentinel_hub import SentinelHubProvider
 
-__all__ = ["FakeSceneProvider", "SceneProvider"]
+__all__ = ["FakeSceneProvider", "SceneProvider", "SentinelHubProvider"]
